d6e5ce8bf5eae70c582e7ef5f1648856938f8cc7
Francisco <Gimli22@gmail.com>
2022-08-03 14:34:47 +0200

    exercises Js

---
+++ index.js
@@ -0,0 +1,20 @@
+document.write('Hello World');
+
+document.write("<br>");
+
+    const num1 = 3
+    const num2 = 5
+    suma = num1 + num2;
+
+document.write(suma);
+
+document.write("<br>");
+
+var nombre = prompt("Escribe tu nombre");
+document.write("Hola "+ nombre);
+
+document.write("<br>");
+
+var n1 = prompt("Escribe un número");
+var n2 = prompt("Escribe otro número");
+document.write("La suma es: "+ ( parseInt(n1)+ parseInt(n2) ) );
